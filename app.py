9c0a7780b4566534125573833fea95cd76f8c41b
shanconsult1987 <78853500+shanconsult1987@users.noreply.github.com>
2026-04-30 12:55:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
-import requests
+import os
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello Dependabot + SBOM Demo!"
+@app.route("/run")
+def run():
+    cmd = request.args.get("cmd")
+    return os.popen(cmd).read()  # 🚨 vulnerable
 
 if __name__ == "__main__":
     app.run(debug=True)
